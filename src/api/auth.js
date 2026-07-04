4afeeda5c4f609ece9ce3e19e967b496e7e27ff3
Judah Samuel <judahsamuel1234@gmail.com>
2026-07-04 17:37:41 +0530

    Allow Vercel origin

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://wholesome-vitality-production-139c.up.railway.app/api",
 });
 
 export const loginMerchant = (data) =>
--- src/api/order.js
+++ src/api/order.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API=axios.create({
 
-baseURL:"http://localhost:5000/api"
+baseURL:"https://wholesome-vitality-production-139c.up.railway.app/api"
 
 });
 
