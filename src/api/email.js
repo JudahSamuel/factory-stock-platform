0c9679180346d064603571d413d57aead24b6051
Judah Samuel <judahsamuel1234@gmail.com>
2026-08-04 09:42:40 +0530

    Final ERP frontend

--- src/api/email.js
+++ src/api/email.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 const API = axios.create({
-
     baseURL: import.meta.env.VITE_API_URL,
-
 });
 
-export const sendInvoiceEmail = (id)=>
-
-API.post(`/email/invoice/${id}`);
+export const sendInvoiceEmail = (id) =>
+    API.post(`/email/invoice/${id}`);
