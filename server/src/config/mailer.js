6f68832f7e86da451170dfe928342420baaab038
Judah Samuel <judahsamuel1234@gmail.com>
2026-08-04 10:12:15 +0530

    Final

--- server/src/config/mailer.js
+++ server/src/config/mailer.js
@@ -1,17 +1,13 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-
-    service: "gmail",
-
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,   // IMPORTANT
     auth: {
-
         user: process.env.EMAIL_USER,
-
-        pass: process.env.EMAIL_PASS
-
-    }
-
+        pass: process.env.EMAIL_PASS,
+    },
 });
 
 export default transporter;
